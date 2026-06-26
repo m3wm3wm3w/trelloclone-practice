a9bbb01697091c0aa93a791a79d79830b11ee9b8
Trello Clone Project <project@trello-clone.dev>
2026-06-26 15:03:55 +0300

    исправление: настроены правильные CORS заголовки для production

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
